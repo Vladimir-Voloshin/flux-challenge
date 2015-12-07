876344904efcb13b6387f2f1a1e7a31a344d1125
Boris Doomer <doomer@webinerds.vvoloshin>
2015-12-07 15:22:19 +0200

    planet title change

--- submissions/Voloshin-Vladimir/angular/js/app.js
+++ submissions/Voloshin-Vladimir/angular/js/app.js
@@ -5,39 +5,34 @@
  */
 
 var app = angular.module('mainSection', []);
-app.controller('sithController', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-});
+app.controller('sithController', function($scope) {});
 
 angular.module('mainSection', [
         'ngWebSocket'
     ])
     .factory('planetData', function($websocket) {
-        // Open a WebSocket connection
         var dataStream = $websocket('ws://localhost:4000');
-
         var collection = [];
 
         dataStream.onMessage(function(message) {
             collection.push(JSON.parse(message.data));
         });
 
+        dataStream.onMessage(function(message) {
+            planetData = JSON.parse(message.data)
+            this.scope.id = planetData.id;
+            this.scope.currentPlanet = planetData.name;
+        });
+
         var methods = {
             collection: collection,
             get: function() {
                 dataStream.send(JSON.stringify({ action: 'get' }));
-            },
-            onmessage: function(){
-                console.log(collection);
             }
         };
 
         return methods;
     })
-    .controller('SomeController', function ($scope, planetData) {
+    .controller('sithController', function ($scope, planetData) {
         $scope.planetData = planetData;
     });
-
-
-
